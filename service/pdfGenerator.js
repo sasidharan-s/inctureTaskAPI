bd5b1951dd8f794049b4caa85077fd2ccfc46fc8
Sasidharan <sasidharan@indiprosoftware.in>
2024-04-15 11:51:13 +0530

    changed PDF format

--- service/pdfGenerator.js
+++ service/pdfGenerator.js
@@ -1,8 +1,13 @@
 const puppeteer = require("puppeteer");
 const path = require("path");
+const chromium = require("chrome-aws-lambda");
 
 async function generatePDF(htmlContent, outputPath) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [...chromium.args],
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
   const page = await browser.newPage();
   await page.setContent(htmlContent);
   // await page.pdf({ path: outputPath, format: "A4" });
